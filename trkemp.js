9be294384b819231ee71a1187d8a6bc4e0b75316
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-06-23 13:29:37 -0700

    adding adding to department databse functions

--- trkemp.js
+++ trkemp.js
@@ -43,7 +43,7 @@ function runSearch() {
         case "add a department to your business":
           addDepart();
           break;
-/// view all emp case to view r\employees
+/// view all emp case to view employees
         case "add a role to your department":
           addRole();
           break;
@@ -67,9 +67,14 @@ function addEmp() {
   inquirer
     .prompt([
       {
-        name: "employee",
+        name: "firstname",
         type: "input",
-        message: "what is the name of your employee?",
+        message: "what is the first name of your employee?",
+      },
+      {
+        name: "lastname",
+        type: "input",
+        message: "what is the last name of your employee?",
       },
       {
         name: "role",
@@ -84,26 +89,22 @@ function addEmp() {
     ])
     .then(function (EMP) {
         // ES6 object destructoring
-        const {employee, role, department} = EMP;
+        const {firstname, lastname, role, department} = EMP;
 
       // change prompt from to employee to include first name and last name
-      console.log(employee, role, department);
+     // console.log(firstname, lastname, role, department);
 
-      var query = `insert into employee (first_name, role_id, manager_id) values ("${employee}", ${role}, ${department})`;
+      var query = `insert into employee (first_name, last_name, role_id, manager_id) values ("${firstname}", "${lastname}", ${role}, ${department})`;
       // conn.query("answer.employee")
       //grabs inquirer prompt for name and stores it in prompt and i would call it back with a query
      // conn.query(query, {first_name: answer.name})
 
       conn.query(query, function(err, res){
           if(err) throw err;
-          viewAllEmployees();
-          //runSearch();
-      })
-      // first_name VARCHAR(30),
-      // last_name VARCHAR(30),
-      // role_id int,
-      // manager_id int
+         // viewAllEmployees();
 
+          runSearch();
+      })
       // var query = "SELECT position, song, year FROM top5000 WHERE ?";
       // conn.query(query, { artist: answer.artist }, function (err, res) {
       //     for (var i = 0; i < res.length; i++) {
@@ -113,11 +114,56 @@ function addEmp() {
       // });
     });
 }
+function addDepart(){
+    inquirer
+      .prompt([
+        {
+          name: "id",
+          type: "input",
+          message: "what role does your employee have?",
+        },
+        {
+          name: "department",
+          type: "input",
+          message: "what department is your employee in?",
+        },
+      ])
+      .then(function (DEP) {
+          // ES6 object destructoring
+          const { id, department} = DEP;
+  
+        // change prompt from to employee to include first name and last name
+       // console.log(firstname, lastname, role, department);
+  
+        var query = `insert into department (id, nameD) values ( ${id}, "${department}")`;
+        // conn.query("answer.employee")
+        //grabs inquirer prompt for name and stores it in prompt and i would call it back with a query
+       // conn.query(query, {first_name: answer.name})
+  
+        conn.query(query, function(err, res){
+            if(err) throw err;
+      // viewAllDepartments();
+            runSearch();
+        });
+       
+      });
+  }
 
+    // viewAllDepartments();
+    function viewAllDepartments(){
+        var query = "select * from department";
+        conn.query(query, function(err, res){
+            if(err) throw err;
+            console.table(res)
+        });
+    };
+    
+
+    // viewAllEmployees();
 function viewAllEmployees(){
     var query = "select * from employee";
     conn.query(query, function(err, res){
         if(err) throw err;
         console.table(res)
-    })
-}
+    });
+};
